3cf3d1ec9a3ebebf808208d3a70df430008b196a
Zuo Yue <zuoyue_zero@163.com>
2024-03-16 18:14:41 +0800

    update dashboard

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-
+import Dashboard from './Page/Dashboard/Dashboard';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -10,7 +10,10 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
