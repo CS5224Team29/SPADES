5b12f0211f6a0707718c13147ab4196391f004e0
Zuo Yue <zuoyue_zero@163.com>
2024-04-07 10:32:15 +0800

    Update Parameter.js

--- frontend/src/Utils/Parameter.js
+++ frontend/src/Utils/Parameter.js
@@ -3,7 +3,7 @@ export const GATEWAY_BASE_URL = `https://kpkxx6puy7h4st72awjuaxm2di0xlbnq.lambda
 
 const AMPLIFY_BASE_URL = `https://dev.dzym427ke4wx7.amplifyapp.com`;
 const LOCALHOST_BASE_URL = `http://localhost:3000`;
-const WEB_ENVIRONMENT = LOCALHOST_BASE_URL;
+const WEB_ENVIRONMENT = AMPLIFY_BASE_URL;
 
 export const LOGOUT_URL = `https://spades.auth.us-east-1.amazoncognito.com/oauth2/authorize?response_type=code&client_id=4l27f32rcgmkkh51eu23g7ormr&logout_uri=${WEB_ENVIRONMENT}/logout`;
 export const REDIRECT_URI = `${WEB_ENVIRONMENT}/dashboard`;
